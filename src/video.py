4e224331666d66f78b4a197bc51e43b6f1991000
shwwdnl <shwwdnl@gmail.com>
2023-10-03 17:02:02 +0500

    udate video.py

--- src/video.py
+++ src/video.py
@@ -4,15 +4,26 @@ import os
 class Video(Channel):
     def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = super().get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                 id=self.video_id
-                                                 ).execute()
+        try:
+            self.video_response = super().get_service().videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=self.video_id
+                ).execute()
 
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = f'https://www.youtube.com/video/{self.video_id}'
-        self.views_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
-        self.duration = self.video_response['items'][0]['contentDetails']['duration']
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.video_url = f'https://www.youtube.com/video/{self.video_id}'
+            self.views_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+            self.duration = self.video_response['items'][0]['contentDetails']['duration']
+
+        except IndexError:
+            self.video_id = video_id
+            self.video_response = None
+            self.video_url = None
+            self.title = None
+            self.views_count = None
+            self.like_count = None
+            self.duration = None
 
 
 
